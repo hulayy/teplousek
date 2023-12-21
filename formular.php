b8ba50d4053dfa312f884764bd7939bbb24b9e02
hulayy <74042238+hulayy@users.noreply.github.com>
2023-12-21 14:09:05 +0100

    init

---
+++ formular.php
@@ -0,0 +1,24 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $krestni = $_POST["krestni"];
+        $prijmeni = $_POST["prijmeni"];
+        $email = $_POST["email"];
+        $zprava = $_POST["zprava"];
+
+        $cilovy_email = "prosekadam@gmail.com"; //změnit na svůj
+
+        $subjekt = "[3JM.cz]: Nová zpráva od klienta.";
+        $obsah = "Jméno: $krestni $prijmeni\n";
+        $obsah .= "E-mail: $email\n";
+        $obsah .= "Zpráva:\n$zprava";
+
+        $odeslano = mail($cilovy_email, $subjekt, $obsah);
+
+        if ($odeslano) {
+            echo "Zpráva byla úspěšně odeslána. Ozveme se Vám během následujících několika dnů.";
+        } else {
+            echo "Došlo k chybě při odesílání.";
+        }
+    }
+?>
